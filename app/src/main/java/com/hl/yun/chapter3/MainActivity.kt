a5cd67cbeab43ea806fe36134c4a33ac00f0ad0c
3283152875@qq.com <3283152875@qq.com>
2025-02-23 19:44:42 +0800

    add: 新旧两种版本进行两个Activity的数据互传

--- app/src/main/java/com/hl/yun/chapter3/MainActivity.kt
+++ app/src/main/java/com/hl/yun/chapter3/MainActivity.kt
@@ -9,30 +9,71 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hl.yun.R
 import com.hl.yun.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("data", "onCreate execute")
-//        button1.setOnClickListener {
-//            Toast.makeText(this, "button1", Toast.LENGTH_LONG).show()
-//        }
 
-//        点击跳转第二个activity google推荐
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(mainBinding.root)
-        mainBinding.button1.setOnClickListener {
+        // 新版本启用数据返回方式 返回时的回调函数：second activity 返回时，给first activity传递数据 观察者
+        //        private val launcher =
+        //            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        //                if (it.resultCode == RESULT_OK)
+        //                    Log.d(
+        //                        "FirstActivity",
+        //                        "returned data is ${it.data?.getStringExtra("return_data")}"
+        //                    )
+        //                else Log.d("FirstActivity", "there isn't returned data")
+        //            }
+        //
+        //        launcher.launch(Intent(this, secondActivity::class.java)) 观察者启动第二个activity活动界面
+
+
+        //        button1.setOnClickListener {
+        //            Toast.makeText(this, "button1", Toast.LENGTH_LONG).show()
+        //        }
+
+        //       显式调用点击跳转第二个activity google推荐
+        //        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        //        setContentView(mainBinding.root)
+        //        mainBinding.button1.setOnClickListener {
+        //            val intent = Intent(this, secondActivity::class.java)
+        //            startActivity(intent)
+        //        }
+
+
+        //        隐式调用activity
+        //        button1.setOnClickListener {
+        //            val intent = Intent("com.example.activitytest.ACTION_START")
+        //            intent.addCategory("com.example.activitytest.MY_CATEGORY")
+        //            startActivity(intent)
+        //        }
+
+        // 数据传输到第二个activity
+        button1.setOnClickListener {
             val intent = Intent(this, secondActivity::class.java)
-            startActivity(intent)
+            intent.putExtra("data_1", "Hello, secondActivity")
+            //startActivity(intent)
+            startActivityForResult(intent, 1) // 启动activity并返回数据(旧版本)
         }
-//        隐式调用activity
-//        button1.setOnClickListener {
-//            val intent = Intent("com.example.activitytest.ACTION_START")
-//            intent.addCategory("com.example.activitytest.MY_CATEGORY")
-//            startActivity(intent)
-//        }
+
+
+    }
+
+    //旧版本获取secondActivity返回数据
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            1 -> if (resultCode == RESULT_OK) {
+                val data_2 = data?.getStringExtra("data_2")
+                Log.d("Hello FirstActivity", "secondActivity returned data is ${data_2}")
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/hl/yun/chapter3/secondActivity.kt
+++ app/src/main/java/com/hl/yun/chapter3/secondActivity.kt
@@ -2,6 +2,7 @@ package com.hl.yun.chapter3
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.hl.yun.R
 import kotlinx.android.synthetic.main.activity_second.*
@@ -10,6 +11,10 @@ class secondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        // 获取传递过来的数据
+        val data = intent.getStringExtra("data_1")
+        Log.d("secondActivity", "data_1 is : $data")
         // 隐式调用浏览器 不同app进行跳转
         button2.setOnClickListener {
             // 隐式调用浏览器
@@ -20,5 +25,23 @@ class secondActivity : AppCompatActivity() {
             val intent = Intent(this, ThirdActivity::class.java)
             startActivity(intent)
         }
+
+        // 传递返回数据
+        button2_2.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("data_2", "Hello FirstActivity! 我是secondActivity的数据")
+            setResult(RESULT_OK, intent)
+            finish() // 关闭Activity
+        }
+
+
+    }
+
+    // 点击返回键
+    override fun onBackPressed() {
+        val intent = Intent()
+        intent.putExtra("data_2", "Hello FirstActivity! 我是secondActivity的数据")
+        setResult(RESULT_OK, intent)
+        finish() // 关闭Activity
     }
 }
