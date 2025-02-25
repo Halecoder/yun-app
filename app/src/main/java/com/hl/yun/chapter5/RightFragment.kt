424cdc93d1e0aff9601acf792079d7c4cbddd038
3283152875@qq.com <3283152875@qq.com>
2025-02-25 21:18:12 +0800

    add: Fragment简单注册使用，合并两个Fragment在一个Activity上

---
+++ app/src/main/java/com/hl/yun/chapter5/RightFragment.kt
@@ -0,0 +1,20 @@
+package com.hl.yun.chapter5
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hl.yun.R
+
+class RightFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.right_fragment, container,
+            false
+        )
+    }
+}
