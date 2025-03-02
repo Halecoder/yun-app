33553331b71520e14ab92b02db270f7ec66afe8e
3283152875@qq.com <3283152875@qq.com>
2025-03-02 16:07:29 +0800

    add: Service实现前台图标常驻，应用通知栏

---
+++ app/src/main/java/com/hl/yun/chapter10/FrontServiceActivity.kt
@@ -0,0 +1,12 @@
+package com.hl.yun.chapter10
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hl.yun.R
+
+class FrontServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front_service)
+    }
+}
--- app/src/main/java/com/hl/yun/chapter10/MyService.kt
+++ app/src/main/java/com/hl/yun/chapter10/MyService.kt
@@ -1,10 +1,18 @@
 package com.hl.yun.chapter10
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.hl.yun.R
 
 class MyService : Service() {
     private val tag = "MyService"
@@ -29,6 +37,33 @@ class MyService : Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d(tag, "onCreating")
+
+        //前台图标类似于Lspose通知栏
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "my_service", "前台Service通知",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
+        }
+
+        val intent = Intent(this, FrontServiceActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val notification = NotificationCompat.Builder(this, "my_service")
+            .setContentTitle("This is content title")
+            .setContentText("This is content text")
+            .setSmallIcon(R.drawable.small_icon)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.drawable.large_icon
+                )
+            )
+            .setContentIntent(pi)
+            .build()
+        //上面都是通知的套路， 就这里改了一下子
+        startForeground(1, notification)
     }
 
     //启动Service时候调用
